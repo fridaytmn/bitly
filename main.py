ae5ecdda5f5a9fdbca8d75b65f113ec6381c1c71
Vlad Shvets <shvets7200@gmail.com>
2023-05-06 12:09:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,66 @@
 import requests
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 
 def shorten_link(token, url):
-    link = 'https://api-ssl.bitly.com/v4/shorten'
+
     headers = {'Authorization': f'Bearer {token}'}
+    link = 'https://api-ssl.bitly.com/v4/shorten'
     body = {'long_url': url}
-    short_link = requests.post(link, headers=headers, json=body)
-    short_link.raise_for_status()
-    print(short_link.ok)
-    bitlink = short_link.json()
-    return f'Битлинк: {bitlink['id']}'
+    response = requests.post(link, headers=headers, json=body)
+    response.raise_for_status()
+    link = response.json()['link']
+    return link
+
+
+def get_striped(url):
+    
+    parsed = urlparse(url)
+    scheme = f'{parsed.scheme}://'
+    short_url = parsed.geturl().replace(scheme, '', 1)
+    return short_url
 
 
 def count_clicks(token, bitlink):
-    try:
-        link = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
-        headers = {'Authorization': f'Bearer {token}'}
-        params = {'units': '-1',
-                  'unit_reference': ''}
-        clicks_count = requests.get(link, headers=headers, params=params)
-        clicks_count.raise_for_status()
-        clicks_count = clicks_count.json()
-    except requests.RequestException:
-        return 'Этот битлинк не пренадлежит Вам'
-    return f'Количество переходов: {clicks_count["total_clicks"]}'
+    
+    headers = {'Authorization': f'Bearer {token}'}
+    params = {'units': '-1',
+              'unit_reference': ''}
+    short_link = get_striped(bitlink)
+    link = f'https://api-ssl.bitly.com/v4/bitlinks/{short_link}/clicks/summary'
+    response = requests.get(link, headers=headers, params=params)
+    response.raise_for_status()
+    clicks_count = response.json()['total_clicks']
+
+    return clicks_count
 
 
-def is_bitlink(url):
-    if url.startswith('http://') or url.startswith('https://'):
-        return False
-    else:
-        response = requests.get(f'http://{url}')
-        response.raise_for_status()
-        return response.ok
 
+def is_bitlink(url, token):
+    headers = {'Authorization': f'Bearer {token}'}
+    short_link = get_striped(url)
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{short_link}'
+    response = requests.get(url, headers=headers)
+
+    return response.ok
 
 def main():
+
     load_dotenv()
+    
     token = os.environ['BITLY_TOKEN']
     url = input('Введите ссылку: ')
+
     try:
-        if is_bitlink(url):
-            print(count_clicks(token, url))
+        bitlink = is_bitlink(url, token)
+        if bitlink:
+            print(f'Количество переходов по ссылке: {count_clicks(token, url)}')
         else:
-            print(shorten_link(token, url))
-    except requests.ConnectionError:
-        print('Сайт недоступен')
+            print(f'Ваш Битлинк: {shorten_link(token, url)}')
+    except requests.HTTPError:
+        print('Некорректно введеная ссылка')
     except requests.RequestException:
         print('Неверные данные для запроса')
 
